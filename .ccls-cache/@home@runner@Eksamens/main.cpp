8f1accc54ddda86d580acdcf1bdbad22f46f3d3f
Marks Meškis <98739089+Ojasik@users.noreply.github.com>
2022-06-13 19:57:43 +0000

    Pievienoti mainīgie un pirmais jautājums

--- .ccls-cache/@home@runner@Eksamens/main.cpp
+++ .ccls-cache/@home@runner@Eksamens/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-  
+  string atb1, atb2, atb3, atb4, atb5, atb6, atb7, atb8,   atb9, atb10;
+  int sum=0;
+  cout<<"Tas ir viktorina par temu for cikls C++";
+  cout<<"\n\n\n1. Kas ir for cikls?\na. Cikls ar pecnosacijumu\nb. Cikls ar priekšnosacijumu\nc. Cikls ar skaititāju\nd. Neviens no minetajiem\n\n";
+  cin>>atb1;
+  if(atb1=="c"){
+    sum++;
+  }
+
 }
